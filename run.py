f3a3818598cea3cc1594ea56e0a6cb7834cc5aeb
Alex Watson <dev.iamlolz@gmail.com>
2016-01-12 02:32:07 -0500

    final draft

--- run.py
+++ run.py
@@ -2,15 +2,15 @@
 import mechanize
 import cookielib
 import csv
+import time
 from bs4 import BeautifulSoup
 
 
-
 # create csv to write data to
-#data = csv.writer(open("data.csv", "w"))
+output = csv.writer(open("data.csv", "w"))
 
 # writer header row to csv
-#data.writerow(["url", "casenumber", "plaintiff", "defendant", "courtroom", "time", "calltype"])
+output.writerow(["url", "year", "casenumber", "plaintiff", "defendant", "courtroom", "time", "calltype"])
 
 
 # setup mechanize browser
@@ -33,7 +33,6 @@ br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 # setup user-agent to seem like a human
 br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
-
 # open website
 br.open('http://www.cookcountyclerkofcourt.org/?section=CASEINFOPage&CASEINFOPage=2500')
 
@@ -44,11 +43,22 @@ num = 1
 # create empty dictionary
 mydict = {}
 
-# loop through list storing as lists
+# open list file
 with open("list.csv", mode="r") as f:
+    # setup csv reader to work with list
     reader = csv.reader(f)
+
+    # loop through list row by row
     for row in reader:
-        print row[0], row[1], row[2]
+        # create empty variables
+        d_url = ''
+        d_year = ''
+        d_casenumber = ''
+        d_plaintiff = ''
+        d_defendant = ''
+        d_courtroom = ''
+        d_time = ''
+        d_calltype = ''
 
         # select form
         br.select_form('Criteria')
@@ -61,34 +71,37 @@ with open("list.csv", mode="r") as f:
         # submit form
         br.submit()
 
+        # store response in variable
         data = br.response().read()
 
         # store response in beautifulsoup
         soup = BeautifulSoup(data, "lxml")
 
-        print soup
-        num += 1
+        # store data if it exists
+        data_exists = soup.find('td', width='256')
+
+        # check if data exists and store data if it does
+        if data_exists == None:
+            print 'Line %d skipped' % num
+        else:
+            d_url = d_casenumber = soup.find('tr', bgcolor='#A8C3FF').td.a['href']
+            d_year = row[0]
+            d_casenumber = soup.find('tr', bgcolor='#A8C3FF').td.a.text
+            d_plaintiff = soup.find('tr', bgcolor='#A8C3FF').td.find_next_sibling("td").text
+            d_defendant = soup.find('tr', bgcolor='#A8C3FF').td.find_next_sibling("td").find_next_sibling("td").text
+            d_courtroom = soup.find('tr', bgcolor='#A8C3FF').td.find_next_sibling("td").find_next_sibling("td").find_next_sibling("td").text
+            d_time = soup.find('tr', bgcolor='#A8C3FF').td.find_next_sibling("td").find_next_sibling("td").find_next_sibling("td").find_next_sibling("td").text
+            d_calltype = soup.find('tr', bgcolor='#A8C3FF').td.find_next_sibling("td").find_next_sibling("td").find_next_sibling("td").find_next_sibling("td").find_next_sibling("td").text
+
+            # write data to row
+            output.writerow([d_url, d_year, d_casenumber, d_plaintiff, d_defendant, d_courtroom, d_time, d_calltype])
+
+        # wait for 2 seconds to create delay in between requests
+        time.sleep(2)
+
+        # write to console
         print 'Line %d complete' % num
-
-"""# loop through list and saving data
-for i in info:
-    # select form
-    br.select_form('Criteria')
-
-    # fill form fields
-    br.form['Year'] = '1997'
-    br.form['div'] = 'M1'
-    br.form['number'] = '155682'
-
-    # submit form
-    br.submit()
-
-    # store response in beautifulsoup
-    soup = BeautifulSoup(br.response().read(), "lxml")
-
-    print i
-    print 'Line %d complete' % num
-    num += 1"""
+        num += 1
 
 
 # print results
